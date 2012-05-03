7fbc83b390df279fdd77053865bfde4109a9966b
Pau Oliva <pau@eslack.org>
2012-05-03 16:26:20 +0200

    bump version to 2.0.3

--- lightum.c
+++ lightum.c
@@ -25,7 +25,7 @@
 
 #include "lightum.h"
 
-#define VERSION "2.0.2"
+#define VERSION "2.0.3"
 
 void usage(const char *error) {
 	fprintf(stderr, "lightum v%s - (c)2011 Pau Oliva Fora <pof@eslack.org>\n",VERSION);
