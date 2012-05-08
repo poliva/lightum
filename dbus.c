2f701e5e10c46d18b3d0af765bb418c81d74ced7
Pau Oliva <pau@eslack.org>
2012-05-08 13:46:53 +0200

    initial support for kde solid power management interface when setting the screen backlight value (needs testing)

--- dbus.c
+++ dbus.c
@@ -220,7 +220,7 @@ int dbus_get_screen_backlight_value() {
 	return retval;
 }
 
-int dbus_set_screen_backlight_value(int backlight) {
+int dbus_set_screen_backlight_value_gnome(int backlight) {
 
 	DBusConnection *connection;
 	DBusError error;
@@ -298,3 +298,99 @@ int dbus_set_screen_backlight_value(int backlight) {
 	return retval;
 }
 
+int dbus_set_screen_backlight_value_kde(int backlight) {
+
+	DBusConnection *connection;
+	DBusError error;
+	DBusMessage *message;
+	DBusMessageIter iter;
+	DBusBusType type;
+	DBusMessage *reply;
+
+	int retval=0;
+	const char *name = NULL;
+        char arg[]="org.kde.Solid.PowerManagement.setBrightness";
+        name=arg;
+        const char *dest = "org.kde.Solid.PowerManagement";
+        const char *path = "/org/kde/Solid/PowerManagement"; 
+
+	type = DBUS_BUS_SESSION;
+
+	int reply_timeout;
+	reply_timeout = 2000;
+
+	dbus_error_init (&error);
+	connection = dbus_bus_get (type, &error);
+
+	if (connection == NULL) {
+		fprintf (stderr, "Failed to open connection to message bus: %s\n", error.message);
+		dbus_error_free (&error);
+		return (-1);
+	}
+
+	char *last_dot;
+	last_dot = strrchr (name, '.');
+	*last_dot = '\0';
+
+	message = dbus_message_new_method_call (NULL,path,name,last_dot + 1);
+	dbus_message_set_auto_start (message, TRUE);
+
+	if (message == NULL)
+	{
+		fprintf (stderr, "Couldn't allocate D-Bus message\n");
+		return (-1);
+	}
+
+	if (dest && !dbus_message_set_destination (message, dest))
+	{
+		fprintf (stderr, "Not enough memory\n");
+		return (-1);
+	}
+
+
+	dbus_message_iter_init_append (message, &iter);
+
+	dbus_int32_t int32;
+	int32 = backlight;
+	dbus_message_iter_append_basic (&iter, DBUS_TYPE_INT32, &int32);
+
+	dbus_error_init (&error);
+	reply = dbus_connection_send_with_reply_and_block (connection, message, reply_timeout, &error);
+	if (dbus_error_is_set (&error)) {
+		fprintf (stderr, "Error %s: %s\n",error.name,error.message);
+		return (-1);
+	}
+
+	if (reply)
+	{
+		dbus_message_iter_init (reply, &iter);
+		dbus_bool_t val;
+		dbus_message_iter_get_basic (&iter, &val);
+		retval = val;
+		dbus_message_unref (reply);
+	}
+
+	dbus_message_unref (message);
+	dbus_connection_unref (connection);
+
+	return retval;
+}
+
+int dbus_set_screen_backlight_value(int backlight) {
+
+	int ret=-1;
+
+	ret = dbus_set_screen_backlight_value_gnome(backlight);
+	if (ret == -1) {
+		ret = dbus_set_screen_backlight_value_kde(backlight);
+		if (ret == -1) {
+			fprintf (stderr, "Can't manage screen backlight on this system\n");
+			return (-1);
+		} else {
+			return ret;
+		}
+	} else {
+		return ret;
+	}
+
+}
