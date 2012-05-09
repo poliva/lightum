97e05d71d206be54afa743b915af55273e8d155f
Pau Oliva <pau@eslack.org>
2012-05-09 11:30:24 +0200

    better error message when not able to control backlight

--- dbus.c
+++ dbus.c
@@ -384,8 +384,8 @@ int dbus_set_screen_backlight_value(int backlight) {
 	if (ret == -1) {
 		ret = dbus_set_screen_backlight_value_kde(backlight);
 		if (ret == -1) {
-			fprintf (stderr, "Can't manage screen backlight on this system\n");
-			return (-1);
+			fprintf (stderr, "Can't manage screen backlight on this system.\nPlease disable backlight with config option 'workmode='1' or command line switch '-w 1'.\nIf you believe this is an error, open a bug report: https://github.com/poliva/lightum/issues\n");
+			exit (-1);
 		} else {
 			return ret;
 		}
